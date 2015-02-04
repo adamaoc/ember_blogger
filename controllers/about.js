937225a6030c5f03d6f430296e15f9300c08e0da
adam moore <adamaoc@gmail.com>
2015-02-04 06:15:39 -0600

    adding more functionality to the about and contact pages

--- controllers/about.js
+++ controllers/about.js
@@ -1,7 +1,14 @@
 Blogger.AboutController = Ember.Controller.extend({
+	isPictureShowing: false,
 	actions: {
 		showRealName: function() {
 			alert("What in the world is this crap???");
+		},
+		showPicture: function() {
+			this.set('isPictureShowing', true);
+		},
+		hidePicture: function() {
+			this.set('isPictureShowing', false);
 		}
 	}
 });
